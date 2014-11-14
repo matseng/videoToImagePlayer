8774154e022d9f9e9f9ab2921d7da9c140572b5f
Michael Tseng <matseng@gmail.com>
2014-11-14 09:38:53 -0800

    started to refactor to create private variables and methods

--- src/app.js
+++ src/app.js
@@ -2,114 +2,125 @@
 // var rawTextData = xmlFileLoader("http://localhost:8000/data/base64SingleImage");
 //var rawTextData = xmlFileLoader("./data/base64Images");
 //var imgArr = toImageArray(rawTextData);
+var Sugr = Sugr || {};
+Sugr.support = Sugr.support || {};
 
-var ImagePlayer = function(url, fps, width, frameCount) {
-  this.url = url;
-  this.imagesString;
-  this.imagesArray;
-  this.fps = fps;
-  this.frameCount = frameCount;
-  this.frameIndex = 0;
-  this.paused;
-  this.timer_start;
-  this.imageEl;
-  this.width =  width;
-}
+Sugr.support.imageplayer = (function() {
 
-ImagePlayer.prototype = {
+  var _imagesArray, _frameIndex = 0, _imageEl, _paused, _timerStart;
 
-  play: function() {
-    this._load();
-  },
+  var ImagePlayer = function(url, fps, width, frameCount) {
+    this.url = url;
+    // this.imagesString;
+    // _imagesArray;
+    this.fps = fps;
+    this.frameCount = frameCount;
+    // _frameIndex = 0;
+    // _paused;
+    // _timerStart;
+    // _imageEl;
+    this.width =  width;
+  }
 
-  _base64ImageToString: function(rawTextData) {
-    return rawTextData.split('\n')
-      .filter(function(val) {
-        return val !=+ "";
-      }
-    )
-  },
+  ImagePlayer.prototype = {
 
-  _play: function() {
-    if( !this.imageEl) this._appendFirstImage(this.imagesArray[this.frameIndex]);
-    if(window.performance) console.log(window.performance.now() - this.timer_start);
-    this.imageEl = document.getElementById('imageFromVideo');
-    this.imageEl.width = this.width;
-    function render() {
-      if (this.frameCount && this.frameIndex == this.frameCount) return;
-      if ( this.frameCount && this.frameIndex === this.imagesArray.length) {
-        this.paused = true;
-        console.log("PAUSED to buffer download");
-        return;
-      }
-      setTimeout(function() {
-        this.frameIndex++;
-        window.requestAnimationFrame(render.bind(this));
-      }.bind(this), 1000 / this.fps);
-      this.imageEl.src = "data:image/jpeg;base64," + this.imagesArray[this.frameIndex];
-    };
-    render.call(this);
-    // console.log(this.imagesArray.length);
-  },
+    play: function() {
+      this._load();
+    },
 
-  _appendFirstImage: function(imgString) {
-    var base64String = "data:image/jpeg;base64," + imgString;
-    this.imageEl = document.createElement('img');
-    this.imageEl.id = "imageFromVideo"
-    this.imageEl.src = base64String;
-    document.body.appendChild(this.imageEl);
-  },
-
-  _load: function() {
-    var xhr;
-    this.xhr = xhr = new XMLHttpRequest();
-    xhr.open('GET', this.url, true);
-    xhr.addEventListener('progress', this._updateProgress.bind(this), false);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 2) {
-        if (window.performance && !this.timer_start) this.timer_start = window.performance.now();
-      }
-    }.bind(this);
-    xhr.onload = function() {
-      if(xhr.readyState === 4) {
-        if(xhr.status === 200) {
-          this.frameCount = this.imagesArray.length;
+    _base64ImageToString: function(rawTextData) {
+      return rawTextData.split('\n')
+        .filter(function(val) {
+          return val !=+ "";
         }
-      }
-    }.bind(this);
-    xhr.onerror = function(e) {
-      console.err(xhr.statusText);
-    };
-    xhr.send();
-  },
+      )
+    },
 
-  _updateProgress: (function(oEvent) {
-    var start = 0;
-    var end;
-    var partialArr;
-    var chunkIndex = 0;
-    var tempStr = "";
-    var sum = 0;
-    return function(oEvent) {
-      this.imagesArray = this.imagesArray || [''];
-      if (oEvent.type && this.xhr.responseText.length) {
-        end = this.xhr.responseText.length;
-        partialArr = this._base64ImageToString(this.imagesArray[this.imagesArray.length - 1] + this.xhr.responseText.substring(start, end));
-        this.imagesArray.pop();
-        Array.prototype.push.apply(this.imagesArray, partialArr)
-        start = end;
-        if(chunkIndex === 0 || this.paused) {
-          this.paused = false;
-          this._play();
+    _play: function() {
+      if( !_imageEl) this._appendFirstImage(_imagesArray[_frameIndex]);
+      // if(window.performance) console.log(window.performance.now() - _timerStart);
+      _imageEl = document.getElementById('imageFromVideo');
+      _imageEl.width = this.width;
+      function render() {
+        if (this.frameCount && _frameIndex == this.frameCount) return;
+        if ( this.frameCount && _frameIndex === _imagesArray.length) {
+          _paused = true;
+          console.log("PAUSED to buffer download");
+          return;
         }
-        chunkIndex++;
-        console.log(chunkIndex);  //TODO: why isn't this being logged? 
-      }
-    };
-  })(),
-};
+        setTimeout(function() {
+          _frameIndex++;
+          window.requestAnimationFrame(render.bind(this));
+        }.bind(this), 1000 / this.fps);
+        _imageEl.src = "data:image/jpeg;base64," + _imagesArray[_frameIndex];
+      };
+      render.call(this);
+      // console.log(_imagesArray.length);
+    },
 
-var im = new ImagePlayer("./data/base64Images_bak", 23, "320");
+    _appendFirstImage: function(imgString) {
+      var base64String = "data:image/jpeg;base64," + imgString;
+      _imageEl = document.createElement('img');
+      _imageEl.id = "imageFromVideo"
+      _imageEl.src = base64String;
+      document.body.appendChild(_imageEl);
+    },
+
+    _load: function() {
+      var xhr;
+      this.xhr = xhr = new XMLHttpRequest();
+      xhr.open('GET', this.url, true);
+      xhr.addEventListener('progress', this._updateProgress.bind(this), false);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 2) {
+          if (window.performance && !_timerStart) _timerStart = window.performance.now();
+        }
+      }.bind(this);
+      xhr.onload = function() {
+        if(xhr.readyState === 4) {
+          if(xhr.status === 200) {
+            this.frameCount = _imagesArray.length;
+          }
+        }
+      }.bind(this);
+      xhr.onerror = function(e) {
+        console.err(xhr.statusText);
+      };
+      xhr.send();
+    },
+
+    _updateProgress: (function(oEvent) {
+      var start = 0;
+      var end;
+      var partialArr;
+      var chunkIndex = 0;
+      var tempStr = "";
+      var sum = 0;
+      return function(oEvent) {
+        _imagesArray = _imagesArray || [''];
+        if (oEvent.type && this.xhr.responseText.length) {
+          end = this.xhr.responseText.length;
+          partialArr = this._base64ImageToString(_imagesArray[_imagesArray.length - 1] + this.xhr.responseText.substring(start, end));
+          _imagesArray.pop();
+          Array.prototype.push.apply(_imagesArray, partialArr)
+          start = end;
+          if(chunkIndex === 0 || _paused) {
+            _paused = false;
+            this._play();
+          }
+          chunkIndex++;
+          console.log(chunkIndex);  //TODO: why isn't this being logged? 
+        }
+      };
+    })(),
+  };
+
+  return ImagePlayer;
+
+})()
+
+
+var im = new Sugr.support.imageplayer("./data/base64Images_bak", 23, "320");
 im.play();
 
 // var LENGTH;
