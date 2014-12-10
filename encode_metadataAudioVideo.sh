df5f5a8fc3441aa3c5528cb3d4c6ab24ed6a0fff
Michael Tseng <matseng@gmail.com>
2014-12-09 17:28:01 -0800

    added script for metadata audio and video - but better version in sugr

---
+++ encode_metadataAudioVideo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm data/base64ImagesMySpaceMetadata
+rm videos/*.jpeg
+# ffmpeg -i qa.mp4 -r 30 -vf scale=1280:-1 -qscale:v 1 thumbnails-%05d.jpeg
+ffmpeg -i videos/myspace.mp4 -r 23 -vf scale=640:-1 -qscale:v 15 videos/thumbnails-%05d.jpeg
+ffmpeg -i videos/myspace.mp4 -strict -2 audio/myspace.aac
+
+echo '{"fps":23,"width":320}' | base64 >> data/base64ImagesMySpaceMetadata
+
+base64 audio/myspace.aac > data/base64MySpaceAudio
+
+
+FILES=videos/*.jpeg
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  base64 $f >> data/base64ImagesMySpaceMetadata
+done
